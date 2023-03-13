194703aef91e2809de0b128bc9e4903dc4c307a9
quangchinh186 <quangchinh1122@gmail.com>
2023-03-13 20:08:47 +0700

    tryin

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { React, useState } from 'react'
 import ProfileCard from './ProfileCard'
 
 function Main() {
+  const [personID, setPersonID] = useState(0);
+  const handleChange = () => {
+    setPersonID(personID + 1)
+  }
   return (
-    <ProfileCard/>
+    <div>
+    <ProfileCard id ={personID}/>
+    <button className='no' onClick={handleChange}>〤</button>
+    <button className='yes' onClick={handleChange}>✓</button>
+    </div>
   )
 }
 
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import { React, useState } from 'react'
 import { motion } from "framer-motion";
 import './App.css';
 
 
-function ProfileCard() {
+function ProfileCard(props) {
+
   return (
-    <motion.div className="Card" drag
-                >
-      
+    <motion.div className="Card" drag="x" onDragEnd={
+      (event, info) => {
+        console.log(info.point.x);
+        if(info.point.x > 300){
+          props.id + 1;
+        }
+      }
+    }>
         <div className="img-container">
           image
         </div>
         <div className="card-body text-center">
-          <h5 className="card-title">name</h5>
+          <h5 className="card-title">name, id {props.id}</h5>
           <h4 className="card-text">age</h4>
           <p className="card-text">desc</p>
-            <button className='no' >〤</button>
-            <button className='yes'>✓</button>
+            
         </div>
         
     </motion.div>
