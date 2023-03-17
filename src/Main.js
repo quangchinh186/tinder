eb1abb624fd99fec4b3bea01b3aba6cce03546be
quangchinh186 <quangchinh1122@gmail.com>
2023-03-17 15:12:06 +0700

    basic

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,41 @@
 import { React, useState } from 'react'
 import ProfileCard from './ProfileCard'
+import './App.css';
 
+var per = [
+  {id : -1},
+  {id : 0, fullName : 'A', choice : 'empty', seen: false}, 
+  {id : 1, fullName : 'B', choice : 'empty', seen: false},
+  {id : 2, fullName : 'C', choice : 'empty', seen: false},
+  {id : 3, fullName : 'D', choice : 'empty', seen: false},
+  {id : 4, fullName : 'E', choice : 'empty', seen: false},
+  {id : 5, fullName : 'F', choice : 'empty', seen: false},
+  {id : 6, fullName : 'G', choice : 'empty', seen: false},
+  {id : 7, fullName : 'H', choice : 'empty', seen: false},
+  {id : 8, fullName : 'I', choice : 'empty', seen: false},
+  {id : 9, fullName : 'J', choice : 'empty', seen: false},]
+
+var match = [];
 function Main() {
-  const [personID, setPersonID] = useState(0);
-  const handleChange = () => {
-    setPersonID(personID + 1)
+  const [curr, setCurr] = useState(per.length-1);
+  const handleYes = () => {
+    per[curr].choice = 'yes'
+    match.push(per[curr].id);
+    setCurr(Math.max(curr-1, 0));
+  }
+  const handleNo = () => {
+    per[curr].choice = 'no'
+    setCurr(Math.max(curr-1, 0));
   }
   return (
-    <div>
-    <ProfileCard id ={personID}/>
-    <button className='no' onClick={handleChange}>〤</button>
-    <button className='yes' onClick={handleChange}>✓</button>
+    <div className='main'>
+      {console.log(match)}
+      {curr === 0 ? null : per.map(person => <ProfileCard key = {person.id} person = {person}/>)}
+      <button className='no' onClick={handleNo}>✖</button>
+      <button className='yes' onClick={handleYes}>❤</button>
     </div>
   )
 }
 
 export default Main
+
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -3,14 +3,36 @@ import { motion } from "framer-motion";
 import './App.css';
 
 
-function ProfileCard(props) {
-
+function ProfileCard({person}) {
+  const [x, setX] = useState(0);
+  const [opacity, setShow] = useState(1);
+  if(person.choice === "yes" && opacity !== 0){
+    setX(400);
+    setShow(0);
+    person.seen = true;
+  }
+  if((person.choice === "no" && opacity !== 0)){
+    setX(-400);
+    setShow(0);
+    person.seen = true;
+  }
+  //console.log(choice);
   return (
-    <motion.div className="Card" drag="x" onDragEnd={
+    <motion.div className="Card" drag="x" dragSnapToOrigin={!person.seen} animate={{ x: x, opacity: opacity }}
+    onDragEnd={
       (event, info) => {
-        console.log(info.point.x);
-        if(info.point.x > 300){
-          props.id + 1;
+        console.log(info.offset.x)
+        if(info.offset.x > 70){
+          setX(400);
+          setShow(0);
+          person.seen = true;
+          person.choice = 'yes';
+        }
+        else if(info.offset.x < -70){
+          setX(-400);
+          setShow(0);
+          person.seen = true;
+          person.choice = 'no';
         }
       }
     }>
@@ -18,10 +40,10 @@ function ProfileCard(props) {
           image
         </div>
         <div className="card-body text-center">
-          <h5 className="card-title">name, id {props.id}</h5>
+          <h5 className="card-title">name: {person.fullName}, id: {person.id}</h5>
           <h4 className="card-text">age</h4>
           <p className="card-text">desc</p>
-            
+          
         </div>
         
     </motion.div>
